758b11c6768450d26d8fdc0031d1817739588733
agouy <contact@agouy.com>
2020-11-07 16:47:40 +0100

    package skeleton

---
+++ R/hello_world.R
@@ -0,0 +1,12 @@
+#' Hello world
+#'
+#' This function allows you to print hello world.
+#' @param none No parameters.
+#' @keywords test
+#' @export
+#' @examples
+#' hello_world()
+ 
+hello_world <- function(){
+    print("Hello world")
+}
